1672de2b1e7d6a0e2c72061814e1aae91387f67c
samgio95 <103532385+samgio95@users.noreply.github.com>
2022-05-04 21:32:10 +0200

    Create eventi.js

---
+++ server/eventi.js
@@ -0,0 +1,157 @@
+const eventi = [
+    {
+        "id" : "1",
+        "channel": {
+            "id": 9115,
+            "logo": "https://www.salesforce.com/content/dam/sfdc-docs/www/logos/logo-salesforce.svg",
+            "name": "Salesforce",
+        },
+        "content": {
+            "contentTitle": "PwC - Unicredit",
+            "episodeNumber": 80,
+            "seasonNumber": 1,
+            "url": "https://www.salesforce.com/it/blog/2020/03/master-digital-transformation-2020.html",
+            "imagesMap": [
+                {
+                    "key": "background",
+                    "img": {
+                        "url": "https://www.salesforce.com/content/dam/web/it_it/www/images/BlogIT/Q1FY21/Master_PWC_CEFRIEL.png"
+                    }
+                },
+                {
+                    "key": "cover",
+                    "img": {
+                        "url": "https://www.salesforce.com/content/dam/web/it_it/www/images/BlogIT/Q1FY21/Master_PWC_CEFRIEL.png"
+                    }
+                },
+                {
+                    "key": "scene",
+                    "img": {
+                        "url": "https://www.salesforce.com/content/dam/web/it_it/www/images/BlogIT/Q1FY21/Master_PWC_CEFRIEL.png"
+                    }
+                }
+            ]
+        },
+        "starttime": "2020-05-08T19:15:00Z",
+        "endtime": "2022-05-06T20:20:00Z",
+        "eventTitle": "Digital Transformation Excellence Program",
+        "eventSynopsis": "Il progetto si pone l’obiettivo di formare 25 giovani talenti sulle tematiche più rilevanti del panorama digitale: dall’intelligenza artificiale all’IoT, passando per Blockchain, Robotics e Cyber Security. Alla fine del Master, oltre al rilascio del diploma finale, gli studenti riceveranno anche la certificazione Salesforce Admin.",
+    },
+    {
+        "id" : "2",
+        "channel": {
+            "id": 9115,
+            "logo": "https://upload.wikimedia.org/wikipedia/commons/0/05/PricewaterhouseCoopers_Logo.svg",
+            "name": "PwC",
+        },
+        "content": {
+            "contentTitle": "PwC",
+            "episodeNumber": 1,
+            "seasonNumber": 1,
+            "url": "https://www.pwc.com/it/it.html",
+            "imagesMap": [
+                {
+                    "key": "background",
+                    "img": {
+                        "url": "https://www.moneymag.ch/IMG/arton103026.jpg"
+                    }
+                },
+                {
+                    "key": "cover",
+                    "img": {
+                        "url": "https://www.moneymag.ch/IMG/arton103026.jpg"
+                    }
+                },
+                {
+                    "key": "scene",
+                    "img": {
+                        "url": "https://www.moneymag.ch/IMG/arton103026.jpg"
+                    }
+                }
+            ]
+        },
+        "starttime": "2020-05-08T19:15:00Z",
+        "endtime": "2022-05-06T20:20:00Z",
+        "eventTitle": "PwC Business Services Srl",
+        "eventSynopsis": "L’Experience Consulting di PwC è la practice che mette insieme tutte le competenze multidisciplinari necessarie per offrire un servizio end-to-end, in grado di far vivere all’utente la medesima esperienza su tutti i touchpoint, senza interruzioni da un canale all’altro.",
+    },
+    {
+        "id" : "3",
+        "channel": {
+            "id": 9115,
+            "logo": "https://upload.wikimedia.org/wikipedia/it/thumb/c/cc/UniCredit_logo.svg/1200px-UniCredit_logo.svg.png",
+            "name": "UniCredit",
+        },
+        "content": {
+            "contentTitle": "UniCredit",
+            "episodeNumber": 1,
+            "seasonNumber": 1,
+            "url": "https://www.unicredit.it/it/privati.html",
+            "imagesMap": [
+                {
+                    "key": "background",
+                    "img": {
+                        "url": "https://www.altroconsumo.it/-/media/altroconsumo/images/home/soldi/conti%20correnti/news/unicredit_shu_551825293_1600x900.jpg"
+                    }
+                },
+                {
+                    "key": "cover",
+                    "img": {
+                        "url": "https://www.altroconsumo.it/-/media/altroconsumo/images/home/soldi/conti%20correnti/news/unicredit_shu_551825293_1600x900.jpg"
+                    }
+                },
+                {
+                    "key": "scene",
+                    "img": {
+                        "url": "https://www.altroconsumo.it/-/media/altroconsumo/images/home/soldi/conti%20correnti/news/unicredit_shu_551825293_1600x900.jpg"
+                    }
+                }
+            ]
+        },
+        "starttime": "2020-05-08T19:15:00Z",
+        "endtime": "2022-05-06T20:20:00Z",
+        "eventTitle": "UniCredit",
+        "eventSynopsis": "UniCredit è una banca commerciale paneuropea con un modello di servizio unico nel suo genere in Italia, Germania, Europa centrale e orientale",
+    },
+
+    {
+        "id" : "4",
+        "channel": {
+            "id": 9115,
+            "logo": "https://learning.cefriel.com/pluginfile.php/1/theme_adaptable/logo/1624954605/logo-cefirel.png",
+            "name": "Cefriel - Politecnico di Milano",
+        },
+        "content": {
+            "contentTitle": "Cefriel",
+            "episodeNumber": 1,
+            "seasonNumber": 1,
+            "url": "https://www.cefriel.com/it/",
+            "imagesMap": [
+                {
+                    "key": "background",
+                    "img": {
+                        "url": "https://stream.arkage.it/hubfs/politecnico-di-milano.jpg"
+                    }
+                },
+                {
+                    "key": "cover",
+                    "img": {
+                        "url": "https://stream.arkage.it/hubfs/politecnico-di-milano.jpg"
+                    }
+                },
+                {
+                    "key": "scene",
+                    "img": {
+                        "url": "https://stream.arkage.it/hubfs/politecnico-di-milano.jpg"
+                    }
+                }
+            ]
+        },
+        "starttime": "2020-05-08T19:15:00Z",
+        "endtime": "2022-05-06T20:20:00Z",
+        "eventTitle": "Cefriel",
+        "eventSynopsis": "Sosteniamo le scelte strategiche delle imprese operando lungo l’intero ciclo dell’innovazione: dagli studi di fattibilità alla realizzazione di prototipi industriali, dal technology scouting all’assessment di nuove tecnologie e soluzioni.",
+    }
+]
+
+module.exports = {eventi}
